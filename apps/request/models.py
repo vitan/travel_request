e9653efaaed7f72cad90df104c669d8f76574b9d
zhou weitao <weizhou@redhat.com>
2012-08-28 16:11:55 +0800

    add request models

--- apps/request/models.py
+++ apps/request/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Add more options if necessary
+STATUS_OPTIONS = ('Processing', 'Approved', 'Postpone', 'Reject')
+NODE_STATUS = list(enumerate(STATUS_OPTIONS))
+
+class Request(models.Model):
+    requestor = models.ForeignKey(User)
+    manager = models.ForeignKey(User)
+    #ccs = models.ForeignKey(User)
+    departure = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    working_dates = models.PositiveIntegerField()
+    reason = models.TextField()
+    status = models.PositiveSmallIntegerField(default=0, choices=NODE_STATUS)
