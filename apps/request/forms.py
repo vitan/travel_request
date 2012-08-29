e5cc198e4796765ba60f7817ff9d05a08bf07012
zhou weitao <weizhou@redhat.com>
2012-08-29 15:41:23 +0800

    change back to static page

--- apps/request/forms.py
+++ apps/request/forms.py
@@ -1,32 +1,13 @@
 from django import forms
 
 class RequestForm(forms.Form):
-    requestor_email = forms.EmailField(
-        label='requestor email',
-    )
-    manager_email = forms.EmailField(
-        label='manager_email',
-    )
-    departure = forms.CharField(
-        label='departure',
-        max_length=255,
-    )
-    destination = forms.CharField(
-        label='destination',
-        max_length=255,
-    )
-    start_date = forms.DateField(
-        label='start date',
-        widget=forms.DateInput({'class': 'datepicker1', })
-    )
-    end_date = forms.DateField(
-        label='end date',
-        widget=forms.DateInput({'class': 'datepicker2', })
-    )
-    working_days = forms.IntegerField(
-        label='total number of working days',
-    )
+    requestor_email = forms.EmailField()
+    manager_email = forms.EmailField()
+    departure = forms.ChoiceField()
+    destination = forms.ChoiceField()
+    start_date = forms.DateField()
+    end_date = forms.DateField()
+    working_days = forms.IntegerField()
     reason = forms.CharField(
-        label='bussiness reason for travel',
         widget=forms.Textarea,
     )
