cf2d5475b9117ae56fb55668db5bc60ec4a73e35
zhou weitao <weizhou@redhat.com>
2012-08-29 14:31:30 +0800

    add send mail function

--- apps/request/views.py
+++ apps/request/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.core.mail import send_mail
 
 from travel_request.apps.request.models import Request
 from travel_request.apps.request.forms import RequestForm
@@ -10,9 +11,22 @@ def travel_request(request, template_name='request/travel-request.html'):
     display the travel_request form
     """
     if request.method == 'POST':
-        form = RequestForm(request.post)
+        form = RequestForm(request.POST)
         if form.is_valid():
-            pass
+            requestor_email = form.cleaned_data['requestor_email']
+            manager_email = form.cleaned_data['manager_email']
+            departure = form.cleaned_data['departure']
+            destination = form.cleaned_data['destination']
+            start_date = form.cleaned_data['start_date']
+            end_date = form.cleaned_data['end_date']
+            working_dates = form.cleaned_data['working_dates']
+            reason = form.cleaned_data['reason']
+
+            send_mail('Travel Request',
+                      '%s,from %s to %s,working off days %s' %(reason, start_date, end_date, working_dates), 
+                      requestor_email,
+                      [manager_email],
+                      fail_silently = False)
         else:
             pass
 
@@ -22,3 +36,4 @@ def travel_request(request, template_name='request/travel-request.html'):
     return render_to_response(template_name, {
         'form': form,
         },context_instance=RequestContext(request))
+
