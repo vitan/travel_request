48d853e8240eb860d446556af5bc279352ee68ea
zhou weitao <weizhou@redhat.com>
2012-08-29 10:45:41 +0800

    set urls

--- urls.py
+++ urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'travel_request.views.home', name='home'),
-    # url(r'^travel_request/', include('travel_request.foo.urls')),
+    url(r'^$', redirect_to, {'url', '/travel_request/'}),
+    url(r'^travel_request/', 'travel_request.apps.request.views.travel_request', name='travel_request'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
