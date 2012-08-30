9bd71de877d82ebf5938cdb1e035938a6fd8fa64
zhou weitao <weizhou@redhat.com>
2012-08-30 16:51:56 +0800

    register request model to admin

---
+++ apps/request/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from travel_request.apps.request.models import Request
+
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('requestor_id',
+                    'manager_id',
+                    'departure',
+                    'destination',
+                    'start_date',
+                    'end_date',
+                    'working_days',
+                    'reason',
+                   'status')
+admin.site.register(Request)
