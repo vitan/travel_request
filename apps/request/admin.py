8459b87d15cf816acf9570a61db93a5aa23a1335
zhou weitao <weizhou@redhat.com>
2012-09-14 14:20:42 +0800

    add request status post save signal

--- apps/request/admin.py
+++ apps/request/admin.py
@@ -1,48 +1,7 @@
 from django.contrib import admin
 from travel_request.apps.request.models import Request, MailConfig
-from travel_request.apps.request.views import ACTION
-from django.core.mail import EmailMultiAlternatives
-from datetime import datetime
 
 class RequestAdmin(admin.ModelAdmin):
-    def save_model(self, request, obj, form, change):
-        if obj.status is not 0:
-            obj.update_date = datetime.now()
-            obj.save()
-
-            subject = "Request Feedback Change"
-            feedback  ="%s at %s <font style='color:red'>%s</font> the following request"\
-                    %(obj.manager_email,
-                      datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                      ACTION[str(obj.status)])
-
-            email_body = feedback+"<br><br>\
-                    From: %s<br>\
-                    Start: %s To: %s<br>\
-                    Working off days: %s<br>\
-                    Reason: %s<br><br>"\
-                    %(obj.requestor_email,
-                      obj.start_date,
-                      obj.end_date,
-                      obj.working_days,
-                      obj.reason)
-
-            #FIXME the manager should be only one? so tos is to
-            tos = [entry.email for entry in MailConfig.objects.filter(is_cc=False)]
-            ccs = [entry.email for entry in MailConfig.objects.filter(is_cc=True)]
-            tos.append(obj.requestor_email)
-
-            msg = EmailMultiAlternatives(subject=subject,
-                                         from_email="hosted-no-reply@redhat.com",
-                                         to=tos,
-                                         cc=ccs,
-                                         headers={'Cc': ','.join(ccs)})
-            
-            msg.attach_alternative(email_body, "text/html")
-            msg.send(fail_silently=False)
-        else:
-            pass
-
     list_display = ('requestor_email',
                     'manager_email',
                     'departure',
--- apps/request/models.py
+++ apps/request/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from django.core.mail import send_mail
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from datetime import datetime
+from django.core.mail import EmailMultiAlternatives
 
 # Add more options if necessary
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 STATUS_OPTIONS = ('Processing', 'Approved', 'Postponed', 'Rejected')
+ACTION = {'1': 'Approve', '2': 'Postpone', '3': 'Reject'}
 NODE_STATUS = list(enumerate(STATUS_OPTIONS))
 
 class Request(models.Model):
@@ -31,3 +34,44 @@ class MailConfig(models.Model):
     
     def __unicode__(self):
         return "%s" % self.username
+
+@receiver(post_save, sender=Request)
+def feedback_handler(sender, **kwargs):
+    obj = kwargs['instance']
+    if obj.status is not 0:
+        subject = "Request Feedback"
+        feedback  ="%s at %s <font style='color:red'>%s</font> the following request"\
+                %(obj.manager_email,
+                  obj.update_date.strftime(DATETIME_FORMAT),
+                  ACTION[str(obj.status)])
+
+        email_body = feedback+"<br><br>\
+                From: %s<br>\
+                Departure: %s<br>\
+                Destination: %s<br>\
+                Start: %s To: %s<br>\
+                Working off days: %s<br>\
+                Reason: %s<br><br>"\
+                %(obj.requestor_email,
+                  obj.departure,
+                  obj.destination,
+                  obj.start_date,
+                  obj.end_date,
+                  obj.working_days,
+                  obj.reason)
+
+        #FIXME the manager should be only one? so tos is to
+        tos = [entry.email for entry in MailConfig.objects.filter(is_cc=False)]
+        ccs = [entry.email for entry in MailConfig.objects.filter(is_cc=True)]
+        tos.append(obj.requestor_email)
+
+        msg = EmailMultiAlternatives(subject=subject,
+                                     from_email="hosted-no-reply@redhat.com",
+                                     to=tos,
+                                     cc=ccs,
+                                     headers={'Cc': ','.join(ccs)})
+        
+        msg.attach_alternative(email_body, "text/html")
+        msg.send(fail_silently=False)
+    else:
+        pass
