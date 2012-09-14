b31ea99d03ce773a954f9220bc66f26e9107a752
zhou weitao <weizhou@redhat.com>
2012-09-14 14:20:42 +0800

    add request status post save signal

--- apps/request/views.py
+++ apps/request/views.py
@@ -7,11 +7,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from hashlib import md5
 from datetime import datetime
 
-from travel_request.apps.request.models import Request, MailConfig
+from travel_request.apps.request.models import Request, MailConfig, ACTION, DATETIME_FORMAT
 
-DATETIME_FORMAT = '%Y-%m-%d'
 HTML_TEMPLATE = "<html><head><title>%s</title></head><body>%s</body></html>"
-ACTION = {'1': 'Approve', '2': 'Postpone', '3': 'Reject'}
 
 def travel_request(request, template_name='request/travel-request.html'):
     """
@@ -19,7 +17,8 @@ def travel_request(request, template_name='request/travel-request.html'):
     """
     if request.method == 'POST':
         form  = request.POST
-        host  = request.META.get('HTTP_REFERER', None) or '/'
+        #host  = request.META.get('HTTP_REFERER', None) or '/'
+        host  = 'http://10.66.3.221:8000/travel_request/'
         title = ""
         body  = ""
         if is_valid(form):
@@ -42,7 +41,7 @@ def travel_request(request, template_name='request/travel-request.html'):
             body  = "your request has been sent to %s at %s<p>\
                     Please check feedback at %s"\
                     %(record.manager_email,
-                      datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                      datetime.now().strftime(DATETIME_FORMAT),
                       record.requestor_email)
         else:
             title = "Form Error"
@@ -69,33 +68,7 @@ def feedback(request, status, feedback_md5):
         q.save()
         html_title = "Request Feedback"
         html_body  ="%s at %s <font style='color:red'>%s</font> the following request"\
-                %(q.manager_email, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), ACTION[status])
-
-        email_body = html_body+"<br><br>\
-                From: %s<br>\
-                Start: %s To: %s<br>\
-                Working off days: %s<br>\
-                Reason: %s<br><br>"\
-                %(q.requestor_email,
-                  q.start_date,
-                  q.end_date,
-                  q.working_days,
-                  q.reason)
-
-        #FIXME the manager should be only one? so tos is to
-        tos = [entry.email for entry in MailConfig.objects.filter(is_cc=False)]
-        ccs = [entry.email for entry in MailConfig.objects.filter(is_cc=True)]
-        tos.append(q.requestor_email)
-
-        msg = EmailMultiAlternatives(subject=html_title,
-                           from_email="hosted-no-reply@redhat.com",
-                           to=tos,
-                           cc=ccs,
-                           headers={'Cc': ','.join(ccs)})
-        
-        msg.attach_alternative(email_body, "text/html")
-        msg.send(fail_silently=False)
-
+                %(q.manager_email, q.update_date.strftime(DATETIME_FORMAT), ACTION[status])
     except ObjectDoesNotExist:
         update_date = Request.objects.get(md5=feedback_md5).update_date
         html_title = "Invalid Url"
@@ -109,12 +82,11 @@ def is_valid(form):
     """
     verify the form's validation
     """
-    for value in form.values():
-        if not value:
-            return False
+    if not all(form.values()):
+        return False
 
-    start = datetime.strptime(form[u'start_date'], DATETIME_FORMAT)
-    end   = datetime.strptime(form[u'end_date'], DATETIME_FORMAT)
+    start = datetime.strptime(form[u'start_date'], DATETIME_FORMAT[:8])
+    end   = datetime.strptime(form[u'end_date'], DATETIME_FORMAT[:8])
     if start > end:
         return False
     return True
@@ -128,15 +100,19 @@ def send_request(record, host):
     subject = 'Travel Request'
     body = "Travel Request<br>\
             From: %s<br>\
+            Departure: %s<br>\
+            Destination: %s<br>\
             Start: %s To: %s<br>\
             Working off days: %s<br>\
             Reason: %s<br><br>\
             Manager will receive another email to feedback the request<br>"\
             %(record.requestor_email,
-            record.start_date,
-            record.end_date,
-            record.working_days,
-            record.reason)
+              record.departure,
+              record.destination,
+              record.start_date,
+              record.end_date,
+              record.working_days,
+              record.reason)
     feedback = '<br>'.join(feedback_urls)
     tos = [entry.email for entry in MailConfig.objects.filter(is_cc=False)]
     ccs = [entry.email for entry in MailConfig.objects.filter(is_cc=True)]
